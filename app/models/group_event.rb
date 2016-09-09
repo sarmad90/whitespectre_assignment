a7dfde22453d76b9c2fe7c929bf5b3b73b1b7d79
sarmad-sabih-10p <sam_sarmad@hotmail.com>
2016-09-09 16:11:45 +0500

    changes

--- app/models/group_event.rb
+++ app/models/group_event.rb
@@ -1,9 +1,28 @@
 class GroupEvent < ActiveRecord::Base
   default_scope { where(deleted: false) } 
-  scope :deleted, -> { where(deleted: true) }
+  scope :deleted, -> { unscoped.where(deleted: true) }
 
-  validates :name, :description, :start, :end, presence: true, if: :published?
+  alias_method :hard_destroy, :destroy # hard_destroy will need to be called to actually delete a record from DB
+
+  validates :name, :description, :start_date, :end_date, presence: true, if: :published?
+  validate :start_date_before_end_date
 
   def publish; update(published: true); end
-  def duration; (start_date - end_date).to_i; end
+  def soft_destroy; update(deleted: true); end
+
+  def duration
+    return "NA" unless start_date && end_date
+    "#{(end_date.to_datetime - start_date.to_datetime).to_f.round(2)} days"
+  end
+
+  def start_date_before_end_date
+    if start_date && end_date && start_date > end_date
+      errors.add(:start_date, "Start date should be before end date")
+    end
+  end
+
+  def destroy
+    update_attribute(:deleted, true)
+    run_callbacks(:destroy)
+  end
 end
